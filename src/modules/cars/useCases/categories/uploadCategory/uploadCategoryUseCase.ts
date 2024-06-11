c3354bccdf6bf618152f0ad27dd148737a151d1c
davi paluch <davipaluch@icloud.com>
2024-06-11 13:42:39 -0300

    lendo um arquivo que foi enviado pela requisição

--- src/modules/cars/useCases/categories/uploadCategory/uploadCategoryUseCase.ts
+++ src/modules/cars/useCases/categories/uploadCategory/uploadCategoryUseCase.ts
@@ -1,7 +1,30 @@
+import fs from "fs";
+import { parse } from "csv-parse";
+
 class UploadCategoryUseCase {
   execute(file: Express.Multer.File): void {
-    console.log(file)
+    const stream = fs.createReadStream(file.path);
+
+    const parser = parse({
+      delimiter: ','
+    });
+
+    stream.pipe(parser); //'.pipe' é responsável por pegar o pedaço lido (linha) do que queremos manipular depois
+
+    parser.on('data', async (line) => {
+      console.log(line);
+    });
+
+    parser.on('end', () => {
+      console.log('CSV file processed successfully.');
+    });
+
+    parser.on('error', (error) => {
+      console.error('Error processing CSV file:', error);
+    });
+
+    //console.log(file);
   }
 }
 
-export { UploadCategoryUseCase }
+export { UploadCategoryUseCase };
