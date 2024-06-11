8b5a874053601962411ceef460c5ee7702daa7b7
davi paluch <davipaluch@icloud.com>
2024-06-11 16:08:48 -0300

    criando método de loadCategories para upload (em andamento).

--- src/modules/cars/useCases/categories/uploadCategory/uploadCategoryUseCase.ts
+++ src/modules/cars/useCases/categories/uploadCategory/uploadCategoryUseCase.ts
@@ -1,25 +1,51 @@
 import fs from "fs";
 import { parse } from "csv-parse";
+import { ICategoryRepository } from "../../../repositories/ICategoriesRepository";
+
+interface IUploadCategory {
+  name: string
+  description: string
+}
 
 class UploadCategoryUseCase {
+  constructor(private categoriesRepository: ICategoryRepository) { }
+
+  loadCategories(file: Express.Multer.File): IUploadCategory[] {
+    const categories: IUploadCategory[] = [];
+
+    const stream = fs.createReadStream(file.path)
+
+    const parseFile = parse({ delimiter: ',' })
+
+    stream.pipe(parseFile)
+
+    parseFile.on("data", async (line) => {
+      const [name, description] = line //desestruturação
+      categories.push({ name, description })
+
+    })
+
+    return categories
+  }
+
   execute(file: Express.Multer.File): void {
     const stream = fs.createReadStream(file.path);
 
-    const parser = parse({
+    const parserFile = parse({
       delimiter: ','
     });
 
-    stream.pipe(parser); //'.pipe' é responsável por pegar o pedaço lido (linha) do que queremos manipular depois
+    stream.pipe(parserFile); //'.pipe' é responsável por pegar o pedaço lido (linha) do que queremos manipular depois
 
-    parser.on('data', async (line) => {
+    parserFile.on('data', async (line) => {
       console.log(line);
     });
 
-    parser.on('end', () => {
+    parserFile.on('end', () => {
       console.log('CSV file processed successfully.');
     });
 
-    parser.on('error', (error) => {
+    parserFile.on('error', (error) => {
       console.error('Error processing CSV file:', error);
     });
 
